5e66e299ccc7035782780a7dcd08c16de8f7ab36
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-10-27 06:47:16 +1300

    Fixed video player and video list

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map( (video) =>
-      <videoListEntry video={video}/>
+      <VideoListEntry video={video}/>
     )}
   </div>
 );
